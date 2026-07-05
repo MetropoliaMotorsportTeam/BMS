e15b3213658c0e3dabc3f7849fadc4696ffd1d9d
DrinkWaterRightNow <minhnguyen23721@gmail.com>
2026-07-05 13:42:22 +0300

    removed config options

--- Core/Src/conf.c
+++ Core/Src/conf.c
@@ -4,74 +4,41 @@
 #include "main.h"
 
 status_data_t status_data = {0};
-limit_t limits = {0};
+limit_t limits = {.max_voltage = 42000,
+                  .min_voltage = 25000,
+                  .max_charge_temp = 4400,
+                  .max_temp = 59,
+                  .min_temp = 0,
+                  .power = (8 * (1e6)),
+                  .tolerance = 0, // Sets max voltage difference
+                  .max_current = 180.0,
+                  .accu_min_voltage = 450.0,
+                  .precharge_min_start_voltage = 450.0,
+                  .precharge_max_end_voltage = 450.0,
+                  .limp_min_voltage = 34000};
 
 void config_1()
 {
   status_data.mode = 0;
-  limits = (limit_t){.max_voltage = 42000,
-                     .min_voltage = 25000,
-                     .max_charge_temp = 4400,
-                     .max_temp = 59,
-                     .min_temp = 0,
-                     .power = (8 * (1e6)),
-                     .tolerance = 0, // Sets max voltage difference
-                     .max_current = 180.0,
-                     .accu_min_voltage = 450.0,
-                     .precharge_min_start_voltage = 450.0,
-                     .precharge_max_end_voltage = 450.0,
-                     .limp_min_voltage = 34000};
+  limits.tolerance = 0;
 }
 
 void config_2()
 {
-  status_data.mode = 1;
-  limits = (limit_t){.max_voltage = 42000,
-                     .min_voltage = 25000,
-                     .max_charge_temp = 4400,
-                     .max_temp = 59,
-                     .min_temp = 0,
-                     .power = (8 * (1e6)),
-                     .tolerance = 0, // Sets max voltage difference
-                     .max_current = 180.0,
-                     .accu_min_voltage = 450.0,
-                     .precharge_min_start_voltage = 450.0,
-                     .precharge_max_end_voltage = 450.0,
-                     .limp_min_voltage = 34000};
+  status_data.mode = 0;
+  limits.tolerance = 1000;
 }
 
 void config_3()
 {
   status_data.mode = 1;
-  limits = (limit_t){.max_voltage = 42000,
-                     .min_voltage = 25000,
-                     .max_charge_temp = 4400,
-                     .max_temp = 69,
-                     .min_temp = 0,
-                     .power = 1e6,
-                     .tolerance = 0, // Sets max voltage difference
-                     .max_current = 180.0,
-                     .accu_min_voltage = 450.0,
-                     .precharge_min_start_voltage = 450.0,
-                     .precharge_max_end_voltage = 450.0,
-                     .limp_min_voltage = 34000};
+  limits.tolerance = 10;
 }
 
 void config_4()
 {
   status_data.mode = 1;
-  limits = (limit_t){.max_voltage = 42000,
-                     .min_voltage = 25000,
-                     .max_charge_temp = 4400,
-                     .max_temp = 67,
-                     .min_temp = 0,
-                     .power = 2 * (1e6),
-                     .tolerance = 0, // Sets max voltage difference
-                     .max_current = 180.0,
-                     .accu_min_voltage = 450.0,
-                     .precharge_min_start_voltage = 450.0,
-                     .precharge_max_end_voltage = 450.0,
-                     .limp_min_voltage = 34000};
+  limits.tolerance = 0;
 }
 
 void process_config(uint8_t config)
